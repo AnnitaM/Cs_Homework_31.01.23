959a0e0bf49bbeb8c7acc2c5b9557506f70fb8c7
Annita Maniri <annitk@gmail.com>
2023-01-31 23:20:40 +0200

    add Task_15 является ли день недели выходным

---
+++ Task_15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Clear(); 
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Error");
+    return;
+}
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("It is weekend");
+}
+else
+{
+    Console.WriteLine("It is working day");
+}
+
+    
+
