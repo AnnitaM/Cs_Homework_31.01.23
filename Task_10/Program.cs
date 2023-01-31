0e6e0da098478fcc39c381c6cc684c38784e7f96
Annita Maniri <annitk@gmail.com>
2023-01-31 23:12:58 +0200

    add Task_10 вторая цифра 3х значного числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Enter a number ");
+int num = int.Parse(Console.ReadLine());
+int scndDig = (num % 100 - num % 10) / 10;
+if (num < 100 || num > 999)
+{
+    Console.Write("Error"); return;
+}
+else
+{
+    Console.WriteLine($"Second digit is {scndDig}");
+}
